4fbaa6d8b9be3a038ffe8fef848c655fe2787f62
codifuse <codifuse@gmail.com>
2025-11-08 22:06:38 +0600

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -60,10 +60,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://0.0.0.0:3000",
+    "https://repo-backend-kckq.onrender.com",
 ]
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
